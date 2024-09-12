b50f35b6fa17c7c31e7ac54d7b6016d53027098a
Aiden <156359836+aid123418@users.noreply.github.com>
2024-09-12 17:58:38 -0400

    Update siteo.py

--- .venv/pages/siteo.py
+++ .venv/pages/siteo.py
@@ -12,15 +12,18 @@ with col1:
     st.write("Hello! A little about me: I joined on a account origionaly from 2014 and sadly lost that one."
                    " I now have 2 accounts, aid123418 (my old, mostly unused) And Tea_timetoday1. Im using my main account to create this server-"
                    " as i have been intruiged on people making awesome custom servers. ")
-
+    st.link_button(url="https://www.roblox.com/games/5041144419/SCP-Roleplay", label="SCP: Roleplay link")
+    st.image('images/webp.webp')
 
 with col2:
-    st.write ("You may find our communications server, and the link to SCP: Roleplay. The Links are below.")
-    st.link_button(url="https://www.roblox.com/games/5041144419/SCP-Roleplay", label="SCP: Roleplay link")
+    st.write ("You may find our communications server, and the link to SCP: Roleplay. The Links are below."
+              "If you are here for the applications- Please click the Applicatons and and the name application you want to do."
+              " If you are here to contact me. Use the info on the contact info tab. The rules are on the rules tab as usual. ")
     st.link_button(url="https://discord.com/invite/TgAvQNGubx", label="Site Ω Communications")
+    st.image('images/omega.jpeg')
 
 
-tab1, tab2 = st.tabs(["Applications", "Contact Info"])
+tab1, tab2, tab3 = st.tabs(["Applications", "Contact Info", "Rules"])
 
 
 with tab1:
@@ -30,3 +33,8 @@ with tab1:
 with tab2:
     st.info("Contact Tea_Timetoday1. His discord @ is @aid123418. if you need to email Tea, all emails can be sent to waitwhahappened@gmail.com.")
     st.link_button(url="https://mail.google.com/mail/u/0/#inbox", label="Link to G-mail")
+
+with tab3:
+    if st.button('Rules and Guide to site Ω')
+        st.link_button(url="https://roblox.com/home", label="Roblox TOS")
+        st.link_button(url="https://roblox.com/home", label="Site Ω Rules")
